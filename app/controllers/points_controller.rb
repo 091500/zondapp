2fef9f908d82ca9b4f5bcf7527466f3c83031827
Ivan Romanenko <iromanenko@beltechsoftware.com>
2018-06-18 15:30:19 +0300

    changes

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,17 +1,34 @@
 class PointsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create, :webhook_create]
+  skip_before_action :verify_authenticity_token, only: [:webhook_create]
+  before_action :get_titles, except: [:webhook_create, :destroy]
   
   def index
-    @points = Point.all.to_a
+    permitted_params = permit_params(params)
+    requested_title = permitted_params[:title]
+    title = @titles.include?(requested_title) ? requested_title : @titles.first
+    
+    @selected_title = title
+    @points = Point.where(title: title).to_a
+    @point = Point.new
   end
   
   def create
     point = Point.create(permit_params(params))
     point.save!
+    
+    redirect_to points_path
   end
 
+  def search
+    permitted_params = permit_params(params)
+    requested_title = permitted_params[:title]
+    title = @titles.include?(requested_title) ? requested_title : @titles.first
+    
+    redirect_to action: 'index', title: title
+  end
+  
   def webhook_create
-    point = Point.create(permit_params(params))
+    point = Point.create(permit_webhook_params(params))
     point.save!
     
     render body: nil
@@ -24,16 +41,38 @@ class PointsController < ApplicationController
   end
   
   def json
-    render json: prepare_points_for_google(Point.all.to_a)
+    permitted_params = permit_params(params)
+    requested_title = permitted_params[:title]
+    title = @titles.include?(requested_title) ? requested_title : @titles.first
+    points = Point.where(title: title).to_a
+    
+    render json: {points: prepare_points_for_google(points)}
   end
   
   private
   
+  def get_titles
+    @titles = Point.all.pluck(:title).uniq.to_a
+  end
+  
   def prepare_points_for_google(points)
-    points.map { |point| {id: point.id, lat: point.lat, lng: point.lon, title: point.title} }
+    points.map do |point| 
+      {
+        id: point.id, 
+        lat: point.lat, 
+        lng: point.lon, 
+        title: point.title, 
+        altitude: point.altitude, 
+        direction: point.direction
+      } 
+    end
   end
   
   def permit_params(params)
-    params.permit(:title, :lat, :lon, :altitude, :direction, :params)
+    params.permit(:title)
+  end
+  
+  def permit_webhook_params(params)
+    params.require(:point).permit(:title, :lat, :lon, :altitude, :direction, :params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   get 'json', to: 'points#json'
   get 'webhook_create', to: 'points#webhook_create'
+  post 'search', to: 'points#search'
   
-  resources :points
+  resources :points, only: [:index, :create, :destroy]
   
   root 'welcome#index'
 end
